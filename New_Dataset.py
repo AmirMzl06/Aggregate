b7d66838100e25c0e899ef1157284b05c3c5121e
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-24 19:21:54 +0330

    Update New_Dataset.py

--- New_Dataset.py
+++ New_Dataset.py
@@ -1,53 +1,85 @@
-import os
-import glob
-import scipy.io as sio
 import numpy as np
 
-class RecogMemoryLoader:
-    def __init__(self, data_path, session, task="NO"):
-        self.data_path = data_path
-        self.session = session
-        self.task = task
-        self.neurons = {}
-        self.events = None
+def extract_trials(events):
+    trials = []
+    STIM_ON = 1
+    stim_indices = np.where(events[:, 1] == STIM_ON)[0]
+    print("Number of stimulus events:", len(stim_indices))
+    for idx in stim_indices:
+        if idx + 3 >= len(events):
+            continue
+        stim_on = events[idx, 0]
+        stim_off = events[idx + 1, 0]
+        question_on = events[idx + 2, 0]
+        response = events[idx + 3, 0]
+        experiment_id = events[idx, 2]
+        trial = {
+            "stim_on": stim_on,
+            "stim_off": stim_off,
+            "question_on": question_on,
+            "response": response,
+            "experiment_id": experiment_id
+        }
+        trials.append(trial)
+    return trials
 
-    def load_neurons(self):
-        neuron_path = os.path.join(self.data_path, "sorted", self.session, self.task)
-        neuron_files = sorted(glob.glob(os.path.join(neuron_path, "*_cells.mat")))
-        print("Neuron folder:")
-        print(neuron_path)
-        print("Number of neurons:", len(neuron_files))
-        for file in neuron_files:
-            neuron_name = os.path.basename(file)
-            neuron_name = neuron_name.replace("_cells.mat", "")
-            mat = sio.loadmat(file)
-            spikes = mat["spikes"]
-            spike_times = spikes[:, 2]
-            self.neurons[neuron_name] = spike_times
+trials = extract_trials(dataset.events)
+print("\nTotal trials:", len(trials))
+for i, t in enumerate(trials[:5]):
+    print("\nTrial", i)
+    for k, v in t.items():
+        print(k, v)
+        
+# import os
+# import glob
+# import scipy.io as sio
+# import numpy as np
 
-    def load_events(self):
-        event_path = os.path.join(self.data_path, "events", self.session, self.task, "eventsRaw.mat")
-        print("\nEvent file:")
-        print(event_path)
-        mat = sio.loadmat(event_path)
-        self.events = mat["events"]
+# class RecogMemoryLoader:
+#     def __init__(self, data_path, session, task="NO"):
+#         self.data_path = data_path
+#         self.session = session
+#         self.task = task
+#         self.neurons = {}
+#         self.events = None
 
-    def load(self):
-        self.load_neurons()
-        self.load_events()
-        print("\nLoading finished!")
-        print("Neurons:", len(self.neurons))
-        print("Events:", self.events.shape)
+#     def load_neurons(self):
+#         neuron_path = os.path.join(self.data_path, "sorted", self.session, self.task)
+#         neuron_files = sorted(glob.glob(os.path.join(neuron_path, "*_cells.mat")))
+#         print("Neuron folder:")
+#         print(neuron_path)
+#         print("Number of neurons:", len(neuron_files))
+#         for file in neuron_files:
+#             neuron_name = os.path.basename(file)
+#             neuron_name = neuron_name.replace("_cells.mat", "")
+#             mat = sio.loadmat(file)
+#             spikes = mat["spikes"]
+#             spike_times = spikes[:, 2]
+#             self.neurons[neuron_name] = spike_times
 
-DATA_PATH = "/home/mirzaei/sam/result/Aggregate/data/RecogMemory/Data"
-session = "P10HMH_092206"
-dataset = RecogMemoryLoader(data_path=DATA_PATH, session=session, task="NO")
-dataset.load()
+#     def load_events(self):
+#         event_path = os.path.join(self.data_path, "events", self.session, self.task, "eventsRaw.mat")
+#         print("\nEvent file:")
+#         print(event_path)
+#         mat = sio.loadmat(event_path)
+#         self.events = mat["events"]
 
-print("\nExample neurons:")
-for neuron, spikes in list(dataset.neurons.items())[:3]:
-    print(neuron, "number of spikes:", len(spikes))
-    print(spikes[:5])
+#     def load(self):
+#         self.load_neurons()
+#         self.load_events()
+#         print("\nLoading finished!")
+#         print("Neurons:", len(self.neurons))
+#         print("Events:", self.events.shape)
 
-print("\nFirst events:")
-print(dataset.events[:10])
+# DATA_PATH = "/home/mirzaei/sam/result/Aggregate/data/RecogMemory/Data"
+# session = "P10HMH_092206"
+# dataset = RecogMemoryLoader(data_path=DATA_PATH, session=session, task="NO")
+# dataset.load()
+
+# print("\nExample neurons:")
+# for neuron, spikes in list(dataset.neurons.items())[:3]:
+#     print(neuron, "number of spikes:", len(spikes))
+#     print(spikes[:5])
+
+# print("\nFirst events:")
+# print(dataset.events[:10])
