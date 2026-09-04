4341ea0efe2b30e7aad98904ec929f898c2a89fb
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-09-04 21:08:56 +0330

    Update Acorn_Test.py

--- Acorn_Test.py
+++ Acorn_Test.py
@@ -101,7 +101,7 @@ def compute_adv_epsilon(X):
     print("\nComputing epsilon")
     dist = float(min_l2_distance(torch.from_numpy(X).float()))
     eps = max(dist / 2.0, 1e-6)
-    eps = 0.1
+    # eps = 0.1
     print("min L2 distance:", dist)
     print("epsilon:", eps)
     return eps
