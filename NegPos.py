c73c9dff7b768eb5b0bc9584e7c340cf717edb04
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-09-17 16:01:35 +0330

    Update NegPos.py

--- NegPos.py
+++ NegPos.py
@@ -53,8 +53,8 @@ if missing:
     raise RuntimeError("Wrong CEBRA fork loaded. Missing NegPos arguments: " f"{sorted(missing)}")
 
 PERICH_DATA_DIR = Path("/data/hossein/mm_project/perich_data_valid_final_raw/")
-DATASET_NAME = "M-CO"
-TARGET_DAY = 16
+DATASET_NAME = "C-CO"
+TARGET_DAY = 4
 TARGET_SESSION = f"{DATASET_NAME}{TARGET_DAY}"
 N_NEURONS = None
 N_CCO_SESSIONS = 22
